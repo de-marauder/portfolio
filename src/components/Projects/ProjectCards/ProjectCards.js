eb0ab3acc6fa4550106531d4fe119aac364d7357
de-marauder <ezikegodson@gmail.com>
2022-05-09 16:53:05 +0100

    updated styles

--- src/components/Projects/ProjectCards/ProjectCards.js
+++ src/components/Projects/ProjectCards/ProjectCards.js
@@ -15,7 +15,7 @@ export default function ProjectCards() {
       img={spicy1} 
       desc="An Online Restaurant for Ordering Local Cuisine."
       tech={['React', 'Redux', 'Tailwindcss', 'React-router-dom', 'stripe API']}
-      links={["https://de-marauder.github.io/spicy_soups/", "https://github.com/de-marauder/spicy_soups"]} />
+      links={["https://spicy-soup.vercel.app/", "https://github.com/de-marauder/spicy_soups"]} />
       
       <ProjectCard 
       name="Wordle clone"

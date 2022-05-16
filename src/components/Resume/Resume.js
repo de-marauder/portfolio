0ee9bc01fa13eda91494ccebd44d30a40d202a28
de-marauder <ezikegodson@gmail.com>
2022-05-16 23:56:45 +0100

    Updated resume links

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,18 +1,12 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { AiOutlineDownload } from "react-icons/ai";
-import { saveAs } from "file-saver";
 
-import ResumeImg from "../../assets/images/resume.png";
 import classes from "../Projects/Projects.module.css";
 import classes1 from "../About/About.module.css";
 import classes2 from "./Resume.module.css";
-// import resume from "../../assets/resume.pdf";
 
 export default function Resume() {
-  // const saveFile = () => {
-  //   saveAs("https://drive.google.com/file/d/1MN3ozejC3crc7dsCGYEj7iIq8D10y-db/view?usp=sharing", "resume.pdf");
-  // };
 
   return (
     <motion.div
@@ -25,11 +19,11 @@ export default function Resume() {
       <div className={classes2.Resume}>
         <h1 className={classes.Title}>Resume</h1>
         <div className={classes2.ResumeWrapper}>
-          <a href="https://docs.google.com/document/d/1HDpmbY8s7qLaIyYm92ZZ10H8vGfphV391jEx11TtcaA/edit?usp=sharing" target="_blank" className={classes2.Download}>
+          <a href="https://docs.google.com/document/d/1yFxmVr6XppoKM4wqdAeuKpTXhrKfZ7LzMCMZMb-pxw8/edit?usp=sharing" download target="_blank" rel="noreferrer" className={classes2.Download}>
             <AiOutlineDownload />
             <p className={classes1.P}>Download</p>
           </a>
-          <img className={classes2.ResumeImg} src={ResumeImg} alt="resume" />
+          <img className={classes2.ResumeImg} src="https://drive.google.com/uc?export=view&id=1Ej7dIncDrLgEQJEL-y42rGq8M4ZpbHST" alt="resume" />
         </div>
       </div>
       <div className={`${classes.svgContainer} ${classes2.svg}`}></div>
